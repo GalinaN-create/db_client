f6d8ab30ffb57e7cb0616166dff1cb57d425657c
GalinaN-create <78936085+GalinaN-create@users.noreply.github.com>
2023-08-13 21:36:40 +0300

    db_client

---
+++ db_client/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+REQUIRES = [
+    'records',
+    'structlog',
+    'allure-pytest'
+]
+
+setup(
+    name='db_client',
+    version='0.0.1',
+    packages=['db_client'],
+    url='https://github.com/GalinaN-create/db_client.git',
+    license='MIT',
+    author='Galina Nosova',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='db_client'
+)
